2017c9203eb47cb7b61e6ac14853ca2283ce73e1
Restyled.io <commits@restyled.io>
2020-07-31 13:22:09 +0000

    Restyled by astyle

--- Day 2/max_pairwise_product_DbDK.cpp
+++ Day 2/max_pairwise_product_DbDK.cpp
@@ -16,18 +16,18 @@ long long MaxPairwiseProduct(const std::vector<int>& numbers) {
     return max_product;
 }
 */
-long long MaxPairwiseProductFast(const std::vector<int>& numbers){
-  int n = numbers.size();
+long long MaxPairwiseProductFast(const std::vector<int>& numbers) {
+    int n = numbers.size();
 
-  int max_index1 = -1;
-  for(int i=0; i < n; i++) if ((max_index1 == -1) || (numbers[i] > numbers[max_index1])) max_index1 = i;
+    int max_index1 = -1;
+    for(int i=0; i < n; i++) if ((max_index1 == -1) || (numbers[i] > numbers[max_index1])) max_index1 = i;
 
-  int max_index2 = -1;
-  for (int j = 0; j < n; j++) {
-    if((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2]))) max_index2 = j;
-  }
+    int max_index2 = -1;
+    for (int j = 0; j < n; j++) {
+        if((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2]))) max_index2 = j;
+    }
 
-  return ((long long)(numbers[max_index1])) * numbers[max_index2];
+    return ((long long)(numbers[max_index1])) * numbers[max_index2];
 }
 
 int main() {

cf2892d2b5fcffcc20e1bd19d2a7592f2c95fb58
Restyled.io <commits@restyled.io>
2020-07-31 15:31:49 +0000

    Restyled by black

--- Day 2/max_pairwise_product2_G3.py
+++ Day 2/max_pairwise_product2_G3.py
@@ -1,4 +1,3 @@
-
 def max_pairwise_product_fast(numbers):
     n = len(numbers)
     index = 0
@@ -7,22 +6,22 @@ def max_pairwise_product_fast(numbers):
             index = i
 
     x = numbers[index]
-    numbers[index] = numbers[n-1]
-    numbers[n-1] = x
+    numbers[index] = numbers[n - 1]
+    numbers[n - 1] = x
 
     index = 1
 
-    for i in range(1, n-1):
+    for i in range(1, n - 1):
         if numbers[i] > numbers[index]:
             index = i
 
     x = numbers[index]
-    numbers[index] = numbers[n-2]
-    numbers[n-2] = x
-    return numbers[n-2]*numbers[n-1]
+    numbers[index] = numbers[n - 2]
+    numbers[n - 2] = x
+    return numbers[n - 2] * numbers[n - 1]
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     input_n = int(input())
     input_numbers = [int(x) for x in input().split()]
     print(max_pairwise_product_fast(input_numbers))

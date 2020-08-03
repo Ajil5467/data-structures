922e920d7e7125c1bf99b1283691539bed065ea0
Restyled.io <commits@restyled.io>
2020-08-03 15:57:35 +0000

    Restyled by clang-format

--- Day 2/max_pairwise_product_DbDK.cpp
+++ Day 2/max_pairwise_product_DbDK.cpp
@@ -1,6 +1,6 @@
+#include <algorithm>
 #include <iostream>
 #include <vector>
-#include <algorithm>
 /*
 long long MaxPairwiseProduct(const std::vector<int>& numbers) {
     long long max_product = 0;
@@ -16,28 +16,32 @@ long long MaxPairwiseProduct(const std::vector<int>& numbers) {
     return max_product;
 }
 */
-long long MaxPairwiseProductFast(const std::vector<int>& numbers) {
-    int n = numbers.size();
+long long MaxPairwiseProductFast(const std::vector<int> &numbers) {
+  int n = numbers.size();
 
-    int max_index1 = -1;
-    for(int i=0; i < n; i++) if ((max_index1 == -1) || (numbers[i] > numbers[max_index1])) max_index1 = i;
+  int max_index1 = -1;
+  for (int i = 0; i < n; i++)
+    if ((max_index1 == -1) || (numbers[i] > numbers[max_index1]))
+      max_index1 = i;
 
-    int max_index2 = -1;
-    for (int j = 0; j < n; j++) {
-        if((j != max_index1) && ((max_index2 == -1) || (numbers[j] > numbers[max_index2]))) max_index2 = j;
-    }
+  int max_index2 = -1;
+  for (int j = 0; j < n; j++) {
+    if ((j != max_index1) &&
+        ((max_index2 == -1) || (numbers[j] > numbers[max_index2])))
+      max_index2 = j;
+  }
 
-    return ((long long)(numbers[max_index1])) * numbers[max_index2];
+  return ((long long)(numbers[max_index1])) * numbers[max_index2];
 }
 
 int main() {
-    int n;
-    std::cin >> n;
-    std::vector<int> numbers(n);
-    for (int i = 0; i < n; ++i) {
-        std::cin >> numbers[i];
-    }
+  int n;
+  std::cin >> n;
+  std::vector<int> numbers(n);
+  for (int i = 0; i < n; ++i) {
+    std::cin >> numbers[i];
+  }
 
-    std::cout << MaxPairwiseProductFast(numbers) << "\n";
-    return 0;
+  std::cout << MaxPairwiseProductFast(numbers) << "\n";
+  return 0;
 }

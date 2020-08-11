e689bf6238e44f704419ce773f41d7b17c378c18
Restyled.io <commits@restyled.io>
2020-08-11 08:55:07 +0000

    Restyled by autopep8

--- Day_5/AmalMohanK/fibonacci_AMAL.py
+++ Day_5/AmalMohanK/fibonacci_AMAL.py
@@ -5,12 +5,13 @@ def calc_fib(n):
 
     return calc_fib(n - 1) + calc_fib(n - 2)
 
+
 def calc_fibf(n):
-    a=[0,1]
-    for i in range(2,n+1):
+    a = [0, 1]
+    for i in range(2, n+1):
         a.append(a[i-1]+a[i-2])
     return a[n]
-    
+
 
 n = int(input())
 print(calc_fibf(n))

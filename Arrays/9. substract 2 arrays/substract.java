bc5b8f7a90a756b4cfc7b0415b3a93872a1ba84a
Restyled.io <commits@restyled.io>
2021-10-09 07:02:55 +0000

    Restyled by clang-format

--- Arrays/9. substract 2 arrays/substract.java
+++ Arrays/9. substract 2 arrays/substract.java
@@ -1,20 +1,15 @@
 public class substract {
-     public static Scanner scn = new Scanner(System.in);
+  public static Scanner scn = new Scanner(System.in);
 
-    public static void inverseArray(int[] arr) {
-        int maxEle = -(int)1e9;
-        int minEle = (int) 1e9;
-        for(int ele : arr){
-            maxEle = Math.max(maxEle, ele);
-            minEle = Math.min(minEle, ele);
-            
-        }
-        return maxEle - minEle;
-
+  public static void inverseArray(int[] arr) {
+    int maxEle = -(int)1e9;
+    int minEle = (int)1e9;
+    for (int ele : arr) {
+      maxEle = Math.max(maxEle, ele);
+      minEle = Math.min(minEle, ele);
     }
+    return maxEle - minEle;
+  }
 
-    public static void main(String[] args) {
-
-    }
-    
+  public static void main(String[] args) {}
 }
